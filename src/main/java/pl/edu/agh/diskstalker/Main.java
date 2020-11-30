8cba32134dd3771f2451a001b9d6693efa6a9380
Uladzislau Tumilovich <vladislav.tumilovich@gmail.com>
2020-11-30 14:52:07 +0100

    Add example execution

--- src/main/java/pl/edu/agh/diskstalker/Main.java
+++ src/main/java/pl/edu/agh/diskstalker/Main.java
@@ -1,8 +1,15 @@
 package pl.edu.agh.diskstalker;
 
+import pl.edu.agh.diskstalker.connection.ConnectionProvider;
+import pl.edu.agh.diskstalker.executor.QueryExecutor;
+
+import java.sql.SQLException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        QueryExecutor.create("INSERT INTO Items (ItemID, Path, Type, Size) VALUES (1, '/home', 'root', '4233')");
 
+        ConnectionProvider.close();
     }
 }
