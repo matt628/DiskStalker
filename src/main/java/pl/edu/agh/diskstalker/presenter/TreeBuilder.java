701c117702b715e6a681005da2e93827de49f339
Mateusz <mateuszobrzut628@gmail.com>
2020-12-02 00:41:05 +0100

    tree builder

--- src/main/java/pl/edu/agh/diskstalker/presenter/TreeBuilder.java
+++ src/main/java/pl/edu/agh/diskstalker/presenter/TreeBuilder.java
@@ -3,29 +3,44 @@ package pl.edu.agh.diskstalker.presenter;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TreeBuilder {
     public TreeBuilder() {
     } // dependency injection thish should be a singelton
 
     void buildTree(String path){ // this string should be change to input folder
-//        // tree view
-//        TreeView folderTreeView = new TreeView();
-//        //main root
-//        TreeItem mainRoot = new TreeItem(path);
-//        //getRoot
-//        TreeItem rootPath1 = new TreeItem("rootPath");
-//        folderTreeView.setRoot(rootPath1);
-//        //add childs
-//        rootPath1.getChildren().add(new TreeItem("1son of first"));
-//        TreeItem son = new TreeItem("2son of first");
-//        rootPath1.getChildren().add(son);
-//        son.getChildren().add("son of son");
-//
-//        mainRoot.getChildren().add(rootPath1);
-//
-//        TreeItem rootPath2 = new TreeItem("rootPath");
-//        folderTreeView.setRoot(rootPath2);
-//        mainRoot.getChildren().add(rootPath2);
+        // tree view
+        TreeView<String> treeView = new TreeView<>();
+
+        //main root
+        TreeItem<String> mainRoot = new TreeItem<>(path);
+
+        buildChildrenTree(mainRoot);
+
+
+
     }
+
+    void buildChildrenTree(TreeItem<String> parent){
+        List<String> childs = getChildren(parent.getValue());
+        for(var child : childs){
+            TreeItem<String> childItem = new TreeItem<>(child);
+            buildChildrenTree(childItem);
+            childItem.getChildren().add(childItem);
+        }
+    }
+
+    List<String> getChildren(String path){
+        /* TODO implement getChildren - maybe its good idea to add a getChildren in Item.
+            then we need to create an interface Item for Root and Item class with method getChildren
+         */
+        var childs = new ArrayList<String>();
+        return childs;
+
+    }
+
+
 
 }
--- src/main/java/pl/edu/agh/diskstalker/view/MainGUI.java
+++
@@ -1,4 +0,0 @@
-package pl.edu.agh.diskstalker.view;
-
-public class MainGUI { // probably it will be deleted
-}
