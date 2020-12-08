9af8e9266e82685ccdcf25c3f057413510d85c83
Uladzislau Tumilovich <valdislav.tumilovich@gmail.com>
2020-12-09 00:14:09 +0100

    Fix FolderAnalyzer

--- src/main/java/pl/edu/agh/diskstalker/analyzer/FolderAnalyzer.java
+++ src/main/java/pl/edu/agh/diskstalker/analyzer/FolderAnalyzer.java
@@ -5,11 +5,12 @@ import pl.edu.agh.diskstalker.model.Root;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.*;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.Optional;
 
-import static java.nio.file.FileVisitResult.*;
+import static java.nio.file.FileVisitResult.CONTINUE;
 
 public class FolderAnalyzer extends SimpleFileVisitor<Path> {
 
@@ -19,39 +20,44 @@ public class FolderAnalyzer extends SimpleFileVisitor<Path> {
         this.root = root;
     }
 
-    // Print information about
-    // each type of file.
+    // Save regular files and symbolic links to the database
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attr) {
-        if (attr.isRegularFile()) {
-            String nameWithType = file.getFileName().toString();
-            String name = nameWithType.substring(0, nameWithType.lastIndexOf('.'));
-            String type = nameWithType.substring(nameWithType.lastIndexOf('.'));
-            String path = file.getParent() + File.separator;
+        String nameWithType = file.getFileName().toString();
+        String name = nameWithType.substring(0, nameWithType.lastIndexOf('.'));
+        String type = nameWithType.substring(nameWithType.lastIndexOf('.'));
+        String path = file.getParent() + File.separator;
 
-            Item.create(name, path, type, String.valueOf(attr.size()), root);
-        }
+        Item.create(name, path, type, String.valueOf(attr.size()), root);
         return CONTINUE;
     }
 
-    // Print each directory visited.
+    // Save directories to the database
     @Override
     public FileVisitResult postVisitDirectory(Path dir, IOException exc) {
+        String name = dir.getFileName().toString();
+        String path = dir.getParent() + File.separator;
 
-        System.out.format("Directory: %s%n", dir.toString());
+        if (path.equals(root.getPath()) && name.equals(root.getName())) {
+            return CONTINUE;
+        }
+
+        Item.create(name, path, null, "0", root);
         return CONTINUE;
     }
 
-    public static void main(String[] args) {
-        Path startingDir = Paths.get("C:\\Users\\vladi\\Desktop\\TestFolder");
-        Optional<Root> root = Root.create("TestFolder", "C:\\Users\\vladi\\Desktop\\", "3453", "354645");
-        if (root.isPresent()) {
-            FolderAnalyzer fa = new FolderAnalyzer(root.get());
-            try {
-                Files.walkFileTree(startingDir, fa);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+//    This was quick testing, bigger tests are coming
+//
+//    public static void main(String[] args) {
+//        Path startingDir = Paths.get("C:\\Users\\vladi\\Desktop\\TestFolder");
+//        Optional<Root> root = Root.create("TestFolder", "C:\\Users\\vladi\\Desktop\\", "3453", "354645");
+//        if (root.isPresent()) {
+//            FolderAnalyzer fa = new FolderAnalyzer(root.get());
+//            try {
+//                Files.walkFileTree(startingDir, fa);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
 }
